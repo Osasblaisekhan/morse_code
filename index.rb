ff99ad9a1f24581b9348c28346d26d30589c5d11
khan blise <144198328+Osasblaisekhan@users.noreply.github.com>
2025-06-10 13:56:59 +0100

    added files

--- index.rb
+++ index.rb
@@ -20,12 +20,16 @@ end
 morse_code = '.. --- ..'
 morse_code_two = "-- -.--   -. .- -- ."
 morse_code_three = '.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...'
+morse_code_four = '--- ... .- ...'
 decoded_message = decode_morse(morse_code)
 decoded_message_two = decode_morse(morse_code_two);
 decoded_message_three = decode_morse(morse_code_three);
+decoded_message_four = decode_morse(morse_code_four)
 
 puts decoded_message  # Outputs: "I O I"
 
 puts decoded_message_two
 
 puts decoded_message_three
+
+puts decoded_message_four
